9430600be7fc73a19b4765204a0e9e500cac6ac5
Malatesh-Patil-67 <malateshpatil6767@example.com>
2024-11-17 19:48:23 +0100

    added All required APIs and constants.

---
+++ src/config/constants.js
@@ -0,0 +1,5 @@
+// Backend API base URL
+export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8081/tasks/';
+
+// Default timeout for Axios requests
+export const DEFAULT_TIMEOUT = 5000; // 5 seconds
